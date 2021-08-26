7c9d6e7a111740b6b63a387f9375ec9dee7f2889
Mike Yuan <me@yhndnzj.com>
2021-08-26 17:33:22 +0800

    Use uname -n to get hostname

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -4,7 +4,7 @@ function fish_prompt
 
   # Just calculate these once, to save a few cycles when displaying the prompt
   if not set -q __fish_prompt_hostname
-    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+    set -g __fish_prompt_hostname (uname -n|cut -d . -f 1)
   end
   if not set -q __fish_prompt_char
     switch (id -u)
