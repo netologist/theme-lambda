ef19ff75d1e8346791781a9b7ae2db5e9c9851d4
js402882 <js402882@gmail.com>
2021-08-27 16:31:48 +0200

    Remove commented-out lines

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -16,20 +16,6 @@ function fish_prompt
   end
 
   # Setup colors
-  #use extended color pallete if available
-#if [[ $terminfo[colors] -ge 256 ]]; then
-#    turquoise="%F{81}"
-#    orange="%F{166}"
-#    purple="%F{135}"
-#    hotpink="%F{161}"
-#    limegreen="%F{118}"
-#else
-#    turquoise="%F{cyan}"
-#    orange="%F{yellow}"
-#    purple="%F{magenta}"
-#    hotpink="%F{red}"
-#    limegreen="%F{green}"
-#fi
   set -l normal (set_color normal)
   set -l white (set_color FFFFFF)
   set -l turquoise (set_color 5fdfff)
